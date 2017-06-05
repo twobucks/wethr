a0bba875d94391063b775623a41764442c3547f5
Dino Trojak <dinotrojak@gmail.com>
2017-06-05 14:27:12 +0200

    made it more readable

--- wethr.js
+++ wethr.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 const got = require('got')
 const ora = require('ora')
+const getEmoji = require('./get-emoji.js')
 
 const API_KEY = '315bfb21a64943c67a92e2da0022fdbe'
 
@@ -11,20 +12,28 @@ if (args[0] === '--imperial') {
   isMetric = false
 }
 
-const getEmoji = require('./get-emoji.js')
-
 const spinner = ora('Detecting your location').start()
-got('http://ip-api.com/json').then(response => {
-  const body = JSON.parse(response.body)
-  return [body.city, body.countryCode]
-}).then(([city, country]) => {
+
+function getLocation () {
+  return got('http://ip-api.com/json').then(response => {
+    const body = JSON.parse(response.body)
+    return [body.city, body.countryCode]
+  })
+  .catch(error => {
+    console.log('Error talking with http://ip-api.com/.')
+    console.log('Try running:\n\n $ curl http://ip-api.com/json\n\n')
+    console.log(error)
+  })
+}
+
+function getTemperature ([city, country]) {
   spinner.color = 'yellow'
   spinner.text = 'Loading weather'
 
   const units = isMetric ? 'metric' : 'imperial'
   const weatherURL = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=${units}`
 
-  got(weatherURL).then(response => {
+  return got(weatherURL).then(response => {
     spinner.stop()
 
     const body = JSON.parse(response.body)
@@ -38,8 +47,6 @@ got('http://ip-api.com/json').then(response => {
     console.log(`Try running:\n\n $ curl ${weatherURL}\n\n`)
     console.log(error)
   })
-}).catch(error => {
-  console.log('Error talking with http://ip-api.com/.')
-  console.log('Try running:\n\n $ curl http://ip-api.com/json\n\n')
-  console.log(error)
-})
+}
+
+getLocation().then(getTemperature)
